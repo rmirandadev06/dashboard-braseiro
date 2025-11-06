481f3aa56b2b837b25e79ace475a8122b4bd5be0
rmirandadev06 <rmiranda.dev06@gmail.com>
2025-11-06 15:46:13 -0300

    Força DNS global para ipv4first

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+// --- CORREÇÃO GLOBAL DE IPV4 (ENETUNREACH) ---
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const express = require('express');
 const db = require('./db');
 const app = express();
