0f0d8e19fa4ba67368410a5427b5e279e3ae3e24
rmirandadev06 <rmiranda.dev06@gmail.com>
2026-01-31 22:38:36 -0300

    Fix SSL

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-// /backend/db.js
-const knex = require('knex'); //
+require('dotenv').config();
+const knex = require('knex');
 
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) {
-    throw new Error('Erro fatal: DATABASE_URL não foi definida nas variáveis de ambiente.');
-}
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        // ESTAS TRÊS LINHAS SÃO OBRIGATÓRIAS PRO SUPABASE:
+        ssl: { 
+            rejectUnauthorized: false 
+        } 
+    }
+});
 
-const dbConfig = {
-  client: 'pg', //
-  connection: connectionString,
-  ssl: { rejectUnauthorized: false } //
-};
-
-const connection = knex(dbConfig); //
-module.exports = connection; //
+module.exports = db;
